a9d0e706e00cdf27ab7ab39c01ee900510a38c05
Enduror <teise-rudolf@hotmail.de>
2018-07-30 14:42:56 +0200

    New Scene LevelGenq
    Man kann die verschiedenen Parameter umstellen für höhenunterschiede ect

---
+++ EndlessRunner/Assets/ArenaBoyAssetts/Scripts/NewScripts/PlatFormGen.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlatFormGen : MonoBehaviour {
+
+
+
+
+    [SerializeField]
+    public GameObject player;
+    public Transform generator;
+    public Transform deleter;
+    public Transform platformHolder;
+
+    public int allowedPlatforms;
+
+   
+    public float platformLength;
+    public float abstand;
+
+
+    public int ChargeLevel;
+    public float JumpHight;
+
+
+
+    public GameObject tilePrefabs;
+    public List<GameObject> activeTiles;
+
+   
+   
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+
+        // platformlength ist die hälfte des Colliders der Platform falls man etwas direct ansetzen möchte. // FUnktioniert aber merkwürdig
+
+       // platformLength = (tilePrefabs.GetComponentInChildren<BoxCollider2D>().size.x)/2;
+
+       
+        
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.transform.position.x+30 >= generator.position.x)
+        {
+            SpawnPlatform();
+            DeletePlatform();
+        }
+        
+	}
+
+
+    public void SpawnPlatform()
+    {
+        GameObject go;
+        go = Instantiate(tilePrefabs) as GameObject;
+        go.transform.SetParent(platformHolder);
+
+        go.transform.position = generator.position;
+        generator.position+= new Vector3( abstand + platformLength,  ChargeLevel*JumpHight, 0);
+        activeTiles.Add(go);
+    }
+    public void DeletePlatform()
+    {
+        if (activeTiles.Count > 6)
+        {
+            Destroy(activeTiles[0]);
+            activeTiles.RemoveAt(0);
+        }
+
+    }
+}
---
+++ EndlessRunner/Assets/ArenaBoyAssetts/Scripts/NewScripts/PlayerNachRechtsSchieben.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNachRechtsSchieben : MonoBehaviour {
+    public Transform startPunkt;
+    public float playerVelocity;
+   
+
+    public Vector3 velocity;
+	// Use this for initialization
+	void Start () {
+        transform.position = startPunkt.position;
+       
+        velocity = new Vector3(playerVelocity, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {        
+        transform.position += velocity * Time.deltaTime;        
+	}
+}
